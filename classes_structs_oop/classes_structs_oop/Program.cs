8b0d9be89ca90b7fdf3b6202cc0ba4d93905c5f5
eclevens <ellaclevens@gmail.com>
2024-10-27 15:41:25 -0400

    Created project

---
+++ classes_structs_oop/classes_structs_oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace classes_structs_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
